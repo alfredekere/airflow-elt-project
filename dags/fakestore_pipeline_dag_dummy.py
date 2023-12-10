7e220568b183d5664eab6912501baacb12c40ecd
VICIWUOHA <viciwuoha@gmail.com>
2023-12-10 12:41:19 +0100

    add dummy pipeline

---
+++ dags/fakestore_pipeline_dag_dummy.py
@@ -0,0 +1,20 @@
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+
+@dag(schedule_interval=None, start_date=datetime(2023, 12, 1), catchup=False)
+def fakestore_pipeline_elt():
+    @task
+    def get_data():
+        return [1, 2, 3]
+
+    @task
+    def transform_data(data):
+        return [x * 2 for x in data]
+
+    @task
+    def store_data(data):
+        print(data)
+
+    data = get_data()
+    transformed_data = transform_data(data)
+    store_data(transformed_data)
